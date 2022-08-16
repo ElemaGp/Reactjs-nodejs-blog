5c06cb85ee6f22d02a8fea2840987ce9b38af33a
ElemaGp <elemagodspower@gmail.com>
2022-08-15 23:00:19 -0700

    created the settings page and the write page, and styled them

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 /* I like the simple way he created the project. Just create your folder in Documents, open the folder in vscode
    and in the terminal type "npx create-react-app . " */
@@ -10,7 +12,7 @@ function App() {
     <>
     <div className="App">
         <TopBar />
-        <Single />
+        <Settings />
         
     </div>
     </>

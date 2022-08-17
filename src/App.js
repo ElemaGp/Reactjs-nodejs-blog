dbbcbb59fbde0115b2c4c578a6eb509086ef0454
ElemaGp <elemagodspower@gmail.com>
2022-08-16 22:11:03 -0700

    added login and register

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 /* I like the simple way he created the project. Just create your folder in Documents, open the folder in vscode
    and in the terminal type "npx create-react-app . " */
 
 function App() {
   return (
-    <>
+    <Router>
     <div className="App">
         <TopBar />
-        <Settings />
+        <Switch>
+        <Route exact path="/">
+            <Home />
+          </Route>
+        <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
         
     </div>
-    </>
+    </Router>
   );
 }
 
